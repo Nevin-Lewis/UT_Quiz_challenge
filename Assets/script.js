d54ddcbbfef3a89f10dbed796af8b4be314195c5
Nevin Lewis <lewisnevin@utexas.edu>
2022-10-12 21:56:11 -0500

    added reset button and way to clear highscore form

--- Assets/script.js
+++ Assets/script.js
@@ -7,7 +7,8 @@ var scoreEl = document.querySelector(".score");
 var saved = document.querySelector(".save_close");
 var names = document.getElementById("name_input");
 var scoretrack = document.querySelector("#scores");
-var closes = document.getElementsByClassName("closes")
+var closes = document.querySelector(".closes");
+var resets = document.querySelector(".reset");
 var timercount = 60;
 var count =0;
 var score = 0;
@@ -17,14 +18,14 @@ var highScore = {
 const test = [
     {Question: "Which of the following indicates that A is equivalent to B in both value and type", answers: {A:"A === B", B:"A = B", C:"A == B", D:"A != B"}, correctAnswer:'A'},
     {Question: "What is that starting index value of an Array", answers: {A:"1", B:"2", C:"3", D:"0"}, correctAnswer:'D'},
-    {Question: "What is an element ID identifier?", answers: {A:"@", B:".", C:"#", D:"&"}, correctAnswer:'C'},
-    {Question: "What element do you need to add before using a function to edit style elements?", answers: {A:"(.)", B:"none", C:".css", D:".style"}, correctAnswer:'D'},
-    {Question: "Where should the link to .JS be in HTML?", answers: {A:"At the top", B:"At the bottom", C:"Anywhere", D:"In the body"}, correctAnswer:'B'},
-    {Question: "What phrase will take an if statement to a second option to see if its true?", answers: {A:"else if", B:"next", C:"if false", D:"else"}, correctAnswer:'B'},
-    {Question: "What does querry selector return", answers: {A:"an element", B:"a list", C:"an array", D:"a function"}, correctAnswer:'A'},
-    {Question: "How do you add an a function that occurs after someone does an action", answers: {A:"add a function", B:"add a loop", C:"add an event listener", D:"its impossible"}, correctAnswer:'C'},
-    {Question: "What phrase will add the element to the end of an array", answers: {A:".add", B:".slice", C:".push", D:".array"}, correctAnswer:'C'},
-    {Question: "What text will search for the next subsection of an object", answers: {A:".nextElement", B:".children", C:".subsect", D:".indent"}, correctAnswer:'B'},
+    // {Question: "What is an element ID identifier?", answers: {A:"@", B:".", C:"#", D:"&"}, correctAnswer:'C'},
+    // {Question: "What element do you need to add before using a function to edit style elements?", answers: {A:"(.)", B:"none", C:".css", D:".style"}, correctAnswer:'D'},
+    // {Question: "Where should the link to .JS be in HTML?", answers: {A:"At the top", B:"At the bottom", C:"Anywhere", D:"In the body"}, correctAnswer:'B'},
+    // {Question: "What phrase will take an if statement to a second option to see if its true?", answers: {A:"else if", B:"next", C:"if false", D:"else"}, correctAnswer:'B'},
+    // {Question: "What does querry selector return", answers: {A:"an element", B:"a list", C:"an array", D:"a function"}, correctAnswer:'A'},
+    // {Question: "How do you add an a function that occurs after someone does an action", answers: {A:"add a function", B:"add a loop", C:"add an event listener", D:"its impossible"}, correctAnswer:'C'},
+    // {Question: "What phrase will add the element to the end of an array", answers: {A:".add", B:".slice", C:".push", D:".array"}, correctAnswer:'C'},
+    // {Question: "What text will search for the next subsection of an object", answers: {A:".nextElement", B:".children", C:".subsect", D:".indent"}, correctAnswer:'B'},
 ];
 var correct_Answer = test.map(function (el) { return el.correctAnswer;});
 function hide(){
@@ -118,11 +119,23 @@ function storeScore() {
     document.getElementsByClassName("save_score")[0].style.display = "none";
     document.getElementsByClassName("highscore")[0].style.display = "block"; 
     localStorage.setItem("highScore", JSON.stringify(highScore));};
+   function reset() {
+    var highScore = {
+    name: [],
+    scores: [],};
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+    for (let i=0; i <5; i++){
+        document.getElementById("scores").children[i].textContent = "";
+        document.getElementById("scores").children[i].style.listStyleType = "none";}
+   }
    
-   
-
+function close(){
+    window.location.reload()
+};
 start.addEventListener("click", hide);
 start.addEventListener("click", setquestions);  
 start.addEventListener("click", startTimer);
 setText.addEventListener("click", Start);
 saved.addEventListener('click', storeScore);
+resets.addEventListener('click', reset);
+closes.addEventListener('click', close);
